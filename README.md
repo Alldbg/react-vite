cbdf5d8d2c5a7a7542086ff5958455cc42a2b4ca
Alldbg <b.afellah.2018@gmail.com>
2022-05-23 23:30:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-react-vite-1hhxbv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-vite-1hhxbv)
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Hello World</button>
+    </div>
+  );
+}
+
+export default App;
